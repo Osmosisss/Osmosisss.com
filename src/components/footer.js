c25c48c9731005d9930bf77ebb6df7d6f17d34f9
Osmosisss <ozyosmosis@gmail.com>
2018-09-14 23:54:48 -0600

    created footer.js component

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-space" />
+        <div className="footer-copyright" />
+        <div className="footer-social" />
+      </div>
+    );
+  }
+}
+
+export default Footer;

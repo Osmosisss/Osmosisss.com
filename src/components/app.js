24d717d6b40b523c546050a74ace2ddc96cc2842
Osmosisss <ozyosmosis@gmail.com>
2018-09-16 17:28:39 -0600

    refactored the code to fit the new vision

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import Header from "./header";
-import Footer from "./footer";
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Footer />
+        <Logo />
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="logo">
+        <div className="logo-space">
+          <img src="assets/osmosisss_logo.png" alt="Osmosisss.com" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Logo;

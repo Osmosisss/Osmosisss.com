9197e2a7d42cbd846099b62a7f46128f1d37b830
Osmosisss <ozyosmosis@gmail.com>
2018-09-15 21:43:34 -0600

    created content for footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,16 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
+      <div className="footer-grid">
         <div className="footer-space" />
-        <div className="footer-copyright" />
-        <div className="footer-social" />
+        <div className="footer-copyright">
+          <p> Osmosis Productions</p>
+        </div>
+        <div className="footer-social">
+          <a>Facebook</a>
+          <a>Instagram</a>
+          <a>Spotify</a>
+        </div>
       </div>
     );
   }

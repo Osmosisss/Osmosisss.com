6fa619603b281f87430155bd07ba0c1bb433a52d
Osmosisss <ozyosmosis@gmail.com>
2018-09-20 19:57:50 -0600

    created the about.js component

---
+++ src/components/about-pg.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Logo from "./logo";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="page-wrapper">
+        <Logo />
+      </div>
+    );
+  }
+}
+
+export default About;
